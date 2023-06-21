2ca58ae529c5f961b2f14046750bce5453fc00e3
AKIB ASHFAQ <108029138+Akib43@users.noreply.github.com>
2023-06-21 23:51:06 +0600

    CPP Array File

---
+++ sepret_0s_and_1s_from_an_array.cpp
@@ -0,0 +1,26 @@
+// Write a C++ program to separate 0s and 1s from a given array of values 0 and 1 //
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=10;
+    int nums[n]= {0, 1, 0, 0 , 1, 1, 1, 0, 1, 0},arr[n];
+    int k=0,m=n-1;
+    for(int i=0;i<n;i++){
+        if(nums[i]==1){
+           arr[m]=nums[i];
+           m--;
+        }
+        if(nums[i]==0){
+            arr[k]=nums[i];
+            k++;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
